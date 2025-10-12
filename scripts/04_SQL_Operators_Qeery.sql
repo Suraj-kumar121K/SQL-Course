f808c97ca95c03e8b5f8d8988611dc60a7897bcd
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-13 02:42:52 +0530

    Update 04_SQL_Operators_Qeery.sql

--- scripts/04_SQL_Operators_Qeery.sql
+++ scripts/04_SQL_Operators_Qeery.sql
@@ -1,16 +1,135 @@
 /* ==============================================================================
-   SQL Data Definition Language (DDL)
+   SQL Operators
 -------------------------------------------------------------------------------
-   This guide covers the essential DDL commands used for defining and managing
-   database structures, including creating, modifying, and deleting tables.
-
-   Table of Contents:
-     1. CREATE - Creating Tables
-     2. ALTER - Modifying Table Structure
-     3. DROP - Removing Tables
+   An operator is a symbol or keyword that performs a specific action on one or 
+   more values or variables, known as operands.
+   Type of Operators:
 =================================================================================
 */
 
 /* ============================================================================== 
-   CREATE
+   --> Comparison Operators
+      1. =
+      2. <> !=
+      3. >
+      4. >=
+      5. <
+      6. <=
 =============================================================================== */
+/*              =
+Retrieve all customers from table */
+SELECT *
+FROM customers 
+WHERE country = 'Germany'
+
+/* Retrieve all customers who are not from Germany
+               !=  */
+SELECT *
+FROM customers
+WHERE country != 'Germany' 
+
+/* Retrieve all customers with a score greater than 500
+              >  */
+SELECT * 
+FROM customers
+WHERE score > 500
+
+/* Retrieve all customers with a score of 500 or more
+              >=  */
+SELECT * 
+FROM customers
+WHERE score >= 500
+
+/* Retrieve all customers with a score less than 500
+               < */
+SELECT *
+FROM customers
+WHERE score < 500
+
+/* Retrieve all customers with a score of 500 or less
+              <=  */
+SELECT *
+FROM customers
+WHERE score <= 500
+
+/* ============================================================================== 
+   --> Logical Operators
+      AND:- All condition must be true
+      OR:- At least one condition must be true
+      NOT:- Excludes matching value 
+=============================================================================== */
+
+/*                                    AND
+Retrieve all customers who are from USA and have a score greater than 500  */
+
+SELECT *
+FROM customers
+WHERE country = 'USA' AND score > 500
+
+/*                                    OR
+Retrieve all customers who are either from USA or have a score greater than 500  */
+SELECT *
+FROM customers
+WHERE country = 'USA' OR score > 500
+
+/*                                   NOT
+Retrieve all customers with a score not less than 500  */
+SELECT *
+FROM customers
+WHERE NOT score < 500
+
+/* ============================================================================== 
+   --> Range Operators
+     BETWEEN:- check if a value is within a range
+=============================================================================== */
+/*                                  BETWEEN
+Retrieve all customers whose are falls in the range between 100 and 500  */
+
+SELECT *
+FROM customers
+WHERE score BETWEEN 10 AND 500
+
+SELECT *
+FROM customers
+WHERE score >= 100 AND score <= 500
+
+/* ============================================================================== 
+   --> Membership Operators
+     IN:- check if a value exists in a list
+=============================================================================== */
+
+/*                                     IN
+Retrieve all customers from either Germany or USA */
+SELECT *
+FROM customers
+WHERE country = 'Germany' OR country = 'USA'
+
+SELECT *
+FROM customers
+WHERE country IN ('Germany', 'USA')
+
+/* ============================================================================== 
+   --> Search Operators
+     LIKE:- Search for a pattern in text
+     M%, %in, %r%, b%
+=============================================================================== */
+/*                             M%, %in, %r%, b%        
+Find all customers whose first name start with 'M' */
+SELECT *
+FROM customers
+WHERE first_name LIKE 'M%'
+
+-- Find all customers whose first name end with 'n'
+SELECT *
+FROM customers
+WHERE first_name LIKE '%n'
+
+-- Find all customers whose first name contains 'r'
+SELECT *
+FROM customers
+WHERE first_name LIKE '%r%'
+
+-- Find all customers whose first name has 'r' in the 3rd position
+SELECT *
+FROM customers
+WHERE first_name LIKE '_r%'
