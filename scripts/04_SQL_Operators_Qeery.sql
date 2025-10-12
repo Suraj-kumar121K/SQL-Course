eb82cc4cb25c9194fccf5115d573ae2cd2b9af09
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-13 02:28:17 +0530

    Create 04_SQL_Operators_Qeery.sql

---
+++ scripts/04_SQL_Operators_Qeery.sql
@@ -0,0 +1,16 @@
+/* ==============================================================================
+   SQL Data Definition Language (DDL)
+-------------------------------------------------------------------------------
+   This guide covers the essential DDL commands used for defining and managing
+   database structures, including creating, modifying, and deleting tables.
+
+   Table of Contents:
+     1. CREATE - Creating Tables
+     2. ALTER - Modifying Table Structure
+     3. DROP - Removing Tables
+=================================================================================
+*/
+
+/* ============================================================================== 
+   CREATE
+=============================================================================== */
