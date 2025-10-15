8842a5fe46790be07a62c89aebac16a9b9c7cf47
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-16 03:33:25 +0530

    Create SQL_SET_OPERATORS.sql

---
+++ scripts/SQL_SET_OPERATORS.sql
@@ -0,0 +1,23 @@
+/*                            SET OPERATORS 
+                         WHAT IS UNION OPERATORS
+UNION operator is a set operator used to combine the result sets of two or 
+more SELECT statements into a single result set.
+*/
+
+/*                            SET OPERATORS 
+                         WHAT IS UNIONALL OPERATORS
+The UNION ALL operator in SQL is used to combine the result sets of two or more 
+SELECT statements into a single result set.
+*/
+
+/*                            SET OPERATORS 
+                         WHAT IS EXCEPT OPERATORS
+An EXCEPT operator is a set operator in SQL that returns distinct rows from the 
+first query that are not present in the second query
+*/
+
+/*                            SET OPERATORS 
+                        WHAT IS INTERSECT OPERATORS
+INTERSECT operator is a set operator in SQL and other database management systems that 
+returns only the rows that are common to two or more SELECT statements.
+*/
